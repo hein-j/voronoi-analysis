17998af8c60bd6b5976b855a5fa6f4397384f290
Hein Jeong <hein.jeong@gmail.com>
2021-04-20 15:37:30 -0700

    Complete and style information popup

--- src/information/Information.js
+++ src/information/Information.js
@@ -1,37 +1,58 @@
+import footerDownload from '../assets/footer-download.svg';
+import coefficient from '../assets/coefficient.png';
+import skewness from '../assets/skewness.png';
+import './Information.sass';
+import download from '../assets/download.svg';
+
 function Information () {
 
   return (
-    <div>
+    <div className="information">
       <h1>Usage</h1>
       <ol>
-        <li>
+        <li className="upload-instructions">
         <span>
           Upload a CSV file of the coordinates. The first record must be a header, and it must contain the field names "x" and "y" exactly.
         </span>
-          Download example CSV (github link?)
+        <span>
+          <a href="" download><img className="icon" src={download} alt="download example csv"/></a>Download example CSV
+        </span>
         </li>
         <li>
           Click on any of the boxes for an expanded view.
         </li>
+        <li>
+          Download the visuals and the related data using the <img className="icon" src={footerDownload} alt="download" /> button in the footer.
+        </li>
       </ol>
       <h1>Info</h1>
       <ul>
-        <li>Negative coordinates are welcome.</li>
+        <li>Welcome to Voronoi Analyzer, an app that generates the <a href="https://en.wikipedia.org/wiki/Voronoi_diagram" target="_blank">Voronoi diagram</a> of a given set of coordinates and performs statistical analysis on the areas of the resulting polygons. You can use it, for instance, to examine the density of cells, nuclei, etc. in micrographs. It is not, however, intended to substitute the likes of MATLAB.</li>
         <li>Cells around the edges are artificially clipped for display, but not for calculations: Infinite cells are not factored into the histogram, skewness, or coefficient.</li>
-        <li>The formula for skewness is: where x<sub>i</sub> is the </li>
+        <li>Negative coordinates are welcome.</li>
+        <li className="formula-li">
+          <span>The formula for quantifying skewness is:</span>
+          <img src={skewness} alt="(1/n * sum(from i=1 to n) of(x-sub-i - x-bar)^3) / (1/n * sum(from i=1 to n) of((x-sub-i - x-bar)^2)^(3/2))" />
+          <span>where x<sub>i</sub> is the area of the i<sup>th</sup> domain and x̄ is the sample mean.<span className="asterisk"> *</span></span>
+        </li>
+        <li className="formula-li">
+          <span>The coefficient of clustering is determined by the ratio between the global coefficient of variance and the average local coefficient of variance in domain sizes. The formula is:</span>
+          <img src={coefficient} alt="(n * x-bar) / (sigma-sub-x sum(from i=1 to n) of(a-bar-sub-i / sigma-sub-a-sub-i))"/>
+          <span>where σ<sub>x</sub> is the standard deviation of all the domains, and ā and σ<sub>a<sub>i</sub></sub> are the mean and the standard deviation of the size of neighboring domains of i<sup>th</sup> domain, respectively.<span className="asterisk"> *</span></span>
+        </li>
+        <li>
+          Check out the open source <a target="_blank" href="https://github.com/hein-j/voronoi-analyzer">Github repo</a>. Issues and pull requests welcome.
+        </li>
       </ul>
       <h1>
         Acknowledgements
       </h1>
       <ul>
         <li>
-          Big thanks to the D3.js library for the data visualization tools.
+          Big thanks to the <a href="https://d3js.org" target="_blank">D3.js</a> library for the data visualization tools.
         </li>
         <li>
-          The formulas for skewness and coefficient come from <a target="_blank" href="https://pubmed.ncbi.nlm.nih.gov/26977812/">this open-access publication</a> in ophthalmology. The app was designed with similar research endeavors in mind.
-        </li>
-        <li>
-          Check out the open-source <a target="_blank" href="https://github.com/hein-j/voronoi-analyzer">Github repo</a>. Issues and pull requests welcome.
+          <span className="asterisk">*</span> — The formulae for skewness and coefficient come from <a target="_blank" href="https://pubmed.ncbi.nlm.nih.gov/26977812/">this open-access publication</a> in ophthalmology.
         </li>
       </ul>
     </div>
