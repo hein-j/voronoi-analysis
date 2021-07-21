868a62d2f7e551a38db31bacf763d9445f75971c
Hein Jeong <hein.jeong@gmail.com>
2021-07-21 10:45:36 -0700

    Add tests for pop ups

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,23 +1,65 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import userEvent from '@testing-library/user-event';
+import Downloads from '../downloads/Downloads';
 
+jest.mock('../downloads/Downloads', () => {return function DummyDownloads(props) {
+  return (
+    <div data-testid="downloads" />
+  );
+}})
 
-test('mounts with correct components', () => {
-  render(<App />);
+describe('App', () => {
+  test('mounts with correct components', () => {
+    render(<App />);
+    
+    screen.getByTitle('histogram');
+    
+    screen.getByTitle('diagram');
+    
+    screen.getByText('Skewness');
+    
+    screen.getByText('Coefficient');
+    
+    const calculations = screen.getAllByText((content, element) => {
+      return element.tagName.toLowerCase() === 'span' && content.includes('.')
+    });
+    
+    expect(calculations.length).toBe(2);
+  });
+  
+  test('visualizations yield correct pop ups', () => {
+    render(<App />);
+    userEvent.click(screen.getByTitle('histogram'));
+    expect(screen.getAllByTitle('histogram').length).toBe(2);
 
-  screen.getByTitle('histogram');
+    userEvent.click(screen.getByTitle('diagram'));
+    expect(screen.getAllByTitle('diagram').length).toBe(2);
 
-  screen.getByTitle('diagram');
+    userEvent.click(screen.getByText('Skewness'));
+    expect(screen.getAllByText('Skewness').length).toBe(2);
 
-  screen.getByText('Skewness');
-
-  screen.getByText('Coefficient');
-
-  const calculations = screen.getAllByText((content, element) => {
-    return element.tagName.toLowerCase() === 'span' && content.includes('.')
+    userEvent.click(screen.getByText('Coefficient'));
+    expect(screen.getAllByText('Coefficient').length).toBe(2);
   });
 
-  expect(calculations.length).toBe(2);
-});
+  test('footer buttons yield correct pop ups', () => {
+    render(<App />);
+
+    userEvent.click(screen.getByRole('button', {
+      name: 'download'
+    }))
+
+    screen.getByTestId('downloads');
+
+    userEvent.click(screen.getByRole('button', {
+      name: 'help'
+    }));
+
+    screen.getByText('Usage');
+  });
 
 
+  
+
+})
